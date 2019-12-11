4c9b403620edcbf1d64a2b2e0dffd72b47154fba
tuomasho <34458881+tuomasho@users.noreply.github.com>
2019-12-12 01:02:31 +0200

    Added practice embeded command + few colors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const botconfig = require("./json_files/botconfig.json");
 const Discord = require("discord.js");
+const botconfig = require("./json_files/botconfig.json");
+const colors = require("./json_files/colors.json");
 
 const bot = new Discord.Client({ disableEveryone: true });
 
@@ -19,6 +20,18 @@ bot.on("message", async message => {
 
   if (cmd === `${prefix}hello`) {
     return message.reply(" Hello");
+  }
+
+  if (cmd === `${prefix}embed`) {
+    let sEmbed = new Discord.RichEmbed()
+      .setColor(colors.gryffindor_red)
+      .setTitle("User info")
+      .setAuthor(`${message.guild.name} Info`, message.guild.iconURL)
+      .addField("**Guild Name:**", `${message.guild.name}`, true)
+      .addField("**Guild owner:**", `${message.guild.owner}`, true)
+      .setFooter(`${bot.user.username} | Footer`);
+
+    message.channel.send({ embed: sEmbed });
   }
 });
 
