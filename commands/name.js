a4ac7da3864973dac58970b28e70a7f865267404
tuomasho <34458881+tuomasho@users.noreply.github.com>
2019-12-16 19:34:29 +0200

    Updated MongoDB query

--- commands/name.js
+++ commands/name.js
@@ -15,59 +15,61 @@ module.exports.run = async (bot, message, args) => {
 
   //has first and lastname declared
   if (args.length > 1) {
-    let user = User.find({ discordID: author });
+    User.findOne({ discordID: `${author}` })
+      .lean()
+      .exec((err, user) => {
+        if (user.discordID == author) {
+          console.log("Existing user found");
+        } else {
+          //User is starting the game for the first time
+          const newUser = new User({
+            discordID: author,
+            characterFirstname: args[0],
+            characterLastname: args[1],
+            timesGryffindor: 0,
+            timesHufflepuff: 0,
+            timesRavenclaw: 0,
+            timesSlytherin: 0,
+            familyMoney: 0,
+            familyItems: []
+          });
 
-    if (user.discordID) {
-      console.log("Existing user found");
-    } else {
-      //User is starting the game for the first time
-      const newUser = new User({
-        discordID: author,
-        characterFirstname: args[0],
-        characterLastname: args[1],
-        timesGryffindor: 0,
-        timesHufflepuff: 0,
-        timesRavenclaw: 0,
-        timesSlytherin: 0,
-        familyMoney: 0,
-        familyItems: []
+          newUser.save();
+
+          let newStats = createNewStats(newUser);
+
+          newStats.save().then(() => {
+            let mEmbed = new Discord.RichEmbed()
+              .setColor(colors.purple_dark)
+              .setAuthor(
+                `Hello ${newUser.characterFirstname} ${newUser.characterLastname}!`
+              )
+              .setDescription(`Your current statistics are`)
+              .addField("**XP:**", newStats.xp, true)
+              .addBlankField(true)
+              .addField("**HP:**", newStats.hp, true)
+              .addField("**Strength:**", newStats.strength, true)
+              .addBlankField(true)
+              .addField("**Spells:**", newStats.spells, true)
+              .addField("**Flying:**", newStats.flying, true)
+              .addBlankField(true)
+              .addField("**Bravery:**", newStats.bravery, true)
+              .addField("**Intelligence:**", newStats.intelligence, true)
+              .addBlankField(true)
+              .addField("**Potions:**", newStats.potions, true)
+              .addField("**Herbology:**", newStats.herbology)
+              .addField("**Money:**", newStats.money, true)
+              .addField("**Reputation:**", newStats.reputation, true)
+              .addField(
+                "**!!NOTICE!!**",
+                "You are now ready to be sorted! \n Please type **!sort** to continue"
+              );
+
+            message.reply({ embed: mEmbed });
+            message.delete();
+          });
+        }
       });
-
-      newUser.save();
-
-      let newStats = createNewStats(newUser);
-
-      newStats.save().then(() => {
-        let mEmbed = new Discord.RichEmbed()
-          .setColor(colors.purple_dark)
-          .setAuthor(
-            `Hello ${newUser.characterFirstname} ${newUser.characterLastname}!`
-          )
-          .setDescription(`Your current statistics are`)
-          .addField("**XP:**", newStats.xp, true)
-          .addBlankField(true)
-          .addField("**HP:**", newStats.hp, true)
-          .addField("**Strength:**", newStats.strength, true)
-          .addBlankField(true)
-          .addField("**Spells:**", newStats.spells, true)
-          .addField("**Flying:**", newStats.flying, true)
-          .addBlankField(true)
-          .addField("**Bravery:**", newStats.bravery, true)
-          .addField("**Intelligence:**", newStats.intelligence, true)
-          .addBlankField(true)
-          .addField("**Potions:**", newStats.potions, true)
-          .addField("**Herbology:**", newStats.herbology)
-          .addField("**Money:**", newStats.money, true)
-          .addField("**Reputation:**", newStats.reputation, true)
-          .addField(
-            "**!!NOTICE!!**",
-            "You are now ready to be sorted! \n Please type **!sort** to continue"
-          );
-
-        message.reply({ embed: mEmbed });
-        message.delete();
-      });
-    }
   }
   //has only firstname declared
   else if (args.length == 1) {
