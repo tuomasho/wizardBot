0ac6a9e10a32c7f627124d060f35ba218edbaf97
tuomasho <34458881+tuomasho@users.noreply.github.com>
2019-12-30 03:20:33 +0200

    Added timers.js function file with few function bases

---
+++ functions/misc_functions/timers.js
@@ -0,0 +1,7 @@
+let timers = new Array();
+
+module.exports.addStudyTimer = (author, subject, time) => {};
+
+module.exports.checkForTimer = author => {};
+
+module.exports.removeTimer = author => {};
