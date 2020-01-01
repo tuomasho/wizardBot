f0c4f9f9f513c97b62c10f8e4f6a3d4a947a1cb7
tuomasho <34458881+tuomasho@users.noreply.github.com>
2020-01-02 00:59:33 +0200

    Added addStudyTImer

--- functions/misc_functions/timers.js
+++ functions/misc_functions/timers.js
@@ -1,7 +1,19 @@
+const { studySubject } = require("../../functions/school_functions/subjects");
+const minute = 60000;
+
 let timers = new Array();
 
-module.exports.addStudyTimer = (author, subject, time) => {};
+module.exports.addStudyTimer = (message, authorHouse, subject, time) => {
+  if (time < 721) {
+    setTimeout(() => {
+      studySubject(message, authorHouse, subject, time);
+    }, time * minute);
+  } else {
+  }
+};
 
-module.exports.checkForTimer = author => {};
+module.exports.checkForTimer = author => {
+  timers.forEach(timer => {});
+};
 
 module.exports.removeTimer = author => {};
