01ffeeefd102544f8f54d3c6d55f7591d184e18a
tuomasho <34458881+tuomasho@users.noreply.github.com>
2020-01-02 02:42:51 +0200

    Added couple timer functions

--- functions/misc_functions/timers.js
+++ functions/misc_functions/timers.js
@@ -3,17 +3,49 @@ const minute = 60000;
 
 let timers = new Array();
 
+//User model
+const User = require("../../models/User");
+
 module.exports.addStudyTimer = (message, authorHouse, subject, time) => {
   if (time < 721) {
-    setTimeout(() => {
+    let timeout = setTimeout(() => {
       studySubject(message, authorHouse, subject, time);
+      this.removeTimer(message.author.id);
     }, time * minute);
+
+    var timer = {
+      author: message.author.id,
+      timeout: timeout
+    };
+
+    timers.push(timer);
+
+    User.where({ discordID: `${message.author.id}` })
+      .updateOne({
+        currentActivity: "studying"
+      })
+      .then();
   } else {
+    message.reply(" study length must be 720 or less.");
   }
 };
 
 module.exports.checkForTimer = author => {
-  timers.forEach(timer => {});
+  let found = false;
+  timers.forEach(timer => {
+    if (timer.author == author) {
+      found = true;
+    }
+  });
+
+  return found;
 };
 
-module.exports.removeTimer = author => {};
+module.exports.removeTimer = author => {
+  timers.forEach((timer, index) => {
+    if (timer.author == author) {
+      clearTimeout(timer.timeout);
+      timers.splice(index, 1);
+    }
+  });
+};
