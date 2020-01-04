7ac86b52c247037e9718314460e2ff0f30a3c9ed
tuomasho <34458881+tuomasho@users.noreply.github.com>
2020-01-04 02:12:40 +0200

    Added adventure timer

--- functions/misc_functions/timers.js
+++ functions/misc_functions/timers.js
@@ -1,4 +1,5 @@
 const { studySubject } = require("../../functions/school_functions/subjects");
+const { completeAdventure } = require("./adventure");
 const minute = 60000;
 
 let timers = new Array();
@@ -31,7 +32,36 @@ module.exports.addStudyTimer = (message, authorHouse, subject, time) => {
       })
       .then();
   } else {
-    message.reply(" study length must be 720 or less.");
+    message.reply(" study length must be 720 or less minutes.");
+  }
+};
+
+module.exports.addAdventureTimer = (message, time) => {
+  if (time < 121) {
+    let timeout = setTimeout(() => {
+      completeAdventure(message, time);
+      this.removeTimer(message.author.id);
+      User.where({ discordID: `${message.author.id}` })
+        .updateOne({
+          currentActivity: "doing nothing"
+        })
+        .then();
+    }, time * minute);
+
+    var timer = {
+      author: message.author.id,
+      timeout: timeout
+    };
+
+    timers.push(timer);
+
+    User.where({ discordID: `${message.author.id}` })
+      .updateOne({
+        currentActivity: "adventuring"
+      })
+      .then();
+  } else {
+    message.reply(" you cannot adventure for longer than 120 minutes.");
   }
 };
 
